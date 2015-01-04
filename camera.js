046fb7aadaf3f551584c43015e6a040c27292557
motiooon <gabriel.g.baciu@gmail.com>
2015-01-04 18:23:00 -0500

    camera 6

--- camera.js
+++ camera.js
@@ -12,7 +12,7 @@ app.use('/', express.static(path.join(__dirname, 'stream')));
  
  
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index2.html');
 });
  
 var sockets = {};
